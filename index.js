301b08c999ecf37e79a7ffe8acda05f95f642a70
ScarXLXX <scarxl@yahoo.com>
2018-05-17 22:36:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,26 +3,9 @@ const bot = new Discord.Client();
 
 bot.on('message', (message) => {
 
-    if(message.content == 'I am a capitalist') {
+    if(message.content == 'I am a capitalist', 'The arabs are coming', 'I am a furry', 'I am gay', 'Karl, halp!') {
 		message.channel.send('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
     }
-
-    if(message.content == 'The arabs are coming') {
-        message.channel.send('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
-    }
-
-    if(message.content == 'I am a furry') {
-        message.channel.send('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
-    }
-
-    if(message.content == 'I am gay') {
-        message.channel.send('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
-    }
-    
-    if(message.content == 'Karl, halp!') {
-        message.channel.send('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
-    }
-
 });
 
 bot.login('NDQ2NzQ4NDY5OTMyMTk1ODQw.Dd9jEw.kchXM7KDGrCamMTdwgzE0y5LB9k');
