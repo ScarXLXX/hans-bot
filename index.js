77f4f029a73baebe3d7e65b57dc6156fe69af5ee
ScarXLXX <scarxl@yahoo.com>
2018-05-17 21:39:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+
+    if(message.content == 'Hans, get the flammenwerfer') {
+        message.reply('At once!');
+    }
+
+});
+
+bot.login('NDQ2NzA3MDM1MDUzMDk2OTgx.Dd9UwQ.jhzfEr5Bh52hdgZ7Go6GYm3elGI');
