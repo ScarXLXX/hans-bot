222f50af53ee5421d741d4baddff3840d6363d2e
ScarXLXX <scarxl@yahoo.com>
2018-05-17 22:18:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const bot = new Discord.Client();
 bot.on('message', (message) => {
 
     if(message.content == 'Hans, get the flammenwerfer') {
-		message.channel.sendMessage('Hans, get the flammenwerfer :flammenwerfer:');
+		message.channel.sendMessage('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
     }
 
     if(message.content == 'The arabs are coming') {
-        message.channel.sendMessage('Hans, get the flammenwerfer :flammenwerfer:');
+        message.channel.sendMessage('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
     }
 
     if(message.content == 'I am a furry') {
-        message.channel.sendMessage('Hans, get the flammenwerfer :flammenwerfer:');
+        message.channel.sendMessage('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
     }
 
     if(message.content == 'I am gay') {
-        message.channel.sendMessage('Hans, get the flammenwerfer :flammenwerfer:');
+        message.channel.sendMessage('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
     }
     
     if(message.content == 'Karl, halp!') {
-        message.channel.sendMessage('Hans, get the flammenwerfer :flammenwerfer:');
+        message.channel.sendMessage('Hans, get the flammenwerfer <:flammenwerfer:446752673757265930>');
     }
 
 });
